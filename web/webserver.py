50cb8e9d0ff7404bfd31f74b5e257275b1d705be
revdm <34659568+revdm@users.noreply.github.com>
2023-09-07 16:02:33 -0400

    Update webserver.py

--- web/webserver.py
+++ web/webserver.py
@@ -1,13 +1,13 @@
-import SimpleHTTPServer
-import SocketServer
+import http.server
+import socketserver
 
 class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
-            self.path = '/index.html'
+            self.path = '/web//html/index.html'
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 Handler = MyRequestHandler
-server = SocketServer.TCPServer(('0.0.0.0', 8080), Handler)
+server = socketserver.TCPServer(('0.0.0.0', 8080), Handler)
 
 server.serve_forever()
