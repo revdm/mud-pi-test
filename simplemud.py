b57a6107068f4dcd6a249b80a81f74cfa7b09d56
revdm <34659568+revdm@users.noreply.github.com>
2023-09-07 22:13:07 -0400

    Update simplemud.py

--- simplemud.py
+++ simplemud.py
@@ -87,7 +87,7 @@ while True:
         #mud.send_message(id,'                                                   motd')
         config = configparser.ConfigParser()
         config.read_file(open(r'motd.ini'))
-        path1 = config.get('My Section', 'motd1' +  'spanky')
+        path1 = config.get('My Section', 'motd1' /n 'spanky')
         mud.send_message(id,path1)
         time.sleep(5)
         # send the new player a prompt for their name
